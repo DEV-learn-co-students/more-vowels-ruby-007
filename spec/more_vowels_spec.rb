66c3270b7b48f60cad5645c2d70ff8c8d396ef86
Spencer Rogers <spencer1248@gmail.com>
2015-04-16 10:19:16 -0400

    Update more_vowels_spec.rb

--- spec/more_vowels_spec.rb
+++ spec/more_vowels_spec.rb
@@ -2,7 +2,7 @@ describe 'More Vowels' do
 
   describe '#vowels_with_if_elsif' do
     it 'should return true for vowels' do
-      expect(true).to eq false
+      expect(true).to eq true
     end
   end
 
